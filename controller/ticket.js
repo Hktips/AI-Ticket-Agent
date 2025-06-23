71f821684aeafc61d139b86b35af1a6c86883fd8
hktips <hemant210433@gmail.com>
2025-06-23 18:08:42 +0530

    create get multiple ticket

--- controller/ticket.js
+++ controller/ticket.js
@@ -33,3 +33,50 @@ export const createTicket = async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const getTickets = async (req, res) => {
+  try {
+    const user = req.user;
+    let tickets = [];
+    if (user.role !== "user") {
+      tickets = Ticket.find({})
+        .populate("assignedTo", ["email", "_id"])
+        .sort({ createdAt: -1 });
+    } else {
+      tickets = await Ticket.find({ createdBy: user._id })
+        .select("title description status createdAt")
+        .sort({ createdAt: -1 });
+    }
+    return res.status(200).json(tickets);
+  } catch (error) {
+    console.error("Error fetching tickets", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getTicket = async (req, res) => {
+  try {
+    const user = req.user;
+    let ticket;
+
+    if (user.role !== "user") {
+      ticket = Ticket.findById(req.params.id).populate("assignedTo", [
+        "email",
+        "_id",
+      ]);
+    } else {
+      ticket = Ticket.findOne({
+        createdBy: user._id,
+        _id: req.params.id,
+      }).select("title description status createdAt");
+    }
+
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+    return res.status(404).json({ ticket });
+  } catch (error) {
+    console.error("Error fetching ticket", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
